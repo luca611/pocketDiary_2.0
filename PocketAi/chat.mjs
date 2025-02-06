d242beaf2a8552777e796b857db3193d9de56c2c
Luca Spoldi <spoldi.luca06@outlook.it>
2025-02-06 10:35:54 +0100

    adjustment to ai prompt

--- PocketAi/chat.mjs
+++ PocketAi/chat.mjs
@@ -82,10 +82,10 @@ export async function setStudyPlan(req, res) {
 
         response = await client.chat.completions.create({
             messages: [
-                { role: "system", content: "Receive inputs from the user: start date, termination date, subject, and study frequency (1 to 5). Generate a pure JSON with no special chars or extra text neither tabs and no formatting chars, response with an array named 'notes' containing objects called 'note' to represent individual study sessions. Each 'note' object must include the following fields: - 'title': A string combining the subject and session number. - 'description': A brief description for the session giving advices on what the user should do in that session.  - 'date': The session's date formatted as dd/mm/yyyy.  Distribute the 'note' objects evenly between the start and termination dates based on the provided frequency, where higher frequency values (e.g., 5) result in more frequent notes and lower frequency values (e.g., 1) result in fewer notes. Note that the frequency value does not directly define the exact number of notes but influences their distribution density across the given date range." },
+                { role: "system", content: "Receive inputs from the user: start date, termination date, subject, and study frequency (1 to 5). Generate a pure and valid JSON with no special chars or extra text neither tabs and no formatting chars, response with an array named 'notes' containing objects called 'note' to represent individual study sessions. Each 'note' object must include the following fields: - 'title': A string combining the subject and session number. - 'description': A brief description for the session giving advices on what the user should do in that session.  - 'date': The session's date formatted as mm/dd/yyyy.  Distribute the 'note' objects evenly between the start and termination dates based on the provided frequency, where higher frequency values (e.g., 5) result in more frequent notes and lower frequency values (e.g., 1) result in fewer notes. Note that the frequency value does not directly define the exact number of notes but influences their distribution density across the given date range." },
                 { role: "user", content: "Start Date:" + startDate + "End Date: " + endDate + "Frequency: " + frequency + "Subject: " + subject }
             ],
-            model: "Llama-3.3-70B-Instruct",
+            model: modelName,
             temperature: 1,
             max_tokens: 4096,
             top_p: 1
