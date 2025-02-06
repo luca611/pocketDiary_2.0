f4ada7a358cdba95d145c741eafca45c4e35748d
Luca Spoldi <spoldi.luca06@outlook.it>
2025-02-06 09:32:11 +0100

    switched to llama

--- PocketAi/chat.mjs
+++ PocketAi/chat.mjs
@@ -4,10 +4,10 @@ import { isValidDate } from "../utils/validator.mjs";
 
 const token = process.env["GITHUB_TOKEN"];
 const endpoint = "https://models.inference.ai.azure.com";
-const modelName = "gpt-4o";
+const modelName = "Llama-3.3-70B-Instruct";
 
 export async function getChatCompletion(req, res) {
-
+    
     if (!req.session.logged) {
         sendError(res, "You are not logged in");
         return;
@@ -45,7 +45,7 @@ export async function getChatCompletion(req, res) {
     catch (error) {
         sendServerError(res, "An error occurred while processing the request.");
     }
-    res.status(200).send({ error: '0', message: response.choices[0].message.content });
+    res.status(200).send({ error: '0', message: response.choices[0].message.content});
 }
 
 export async function setStudyPlan(req, res) {
@@ -85,12 +85,13 @@ export async function setStudyPlan(req, res) {
                 { role: "system", content: "Receive inputs from the user: start date, termination date, subject, and study frequency (1 to 5). Generate a pure JSON with no special chars or extra text neither tabs and no formatting chars, response with an array named 'notes' containing objects called 'note' to represent individual study sessions. Each 'note' object must include the following fields: - 'title': A string combining the subject and session number. - 'description': A brief description for the session giving advices on what the user should do in that session.  - 'date': The session's date formatted as dd/mm/yyyy.  Distribute the 'note' objects evenly between the start and termination dates based on the provided frequency, where higher frequency values (e.g., 5) result in more frequent notes and lower frequency values (e.g., 1) result in fewer notes. Note that the frequency value does not directly define the exact number of notes but influences their distribution density across the given date range." },
                 { role: "user", content: "Start Date:" + startDate + "End Date: " + endDate + "Frequency: " + frequency + "Subject: " + subject }
             ],
-            model: "gpt-4o",
+            model: "Llama-3.3-70B-Instruct",
             temperature: 1,
             max_tokens: 4096,
             top_p: 1
         });
 
+        
         let cleanedOutput = JSON.parse(response.choices[0].message.content);
 
         res.status(200).send({ error: '0', message: cleanedOutput });
