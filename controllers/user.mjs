d976d29e4b6494f86d60a557a103a0e6c97b4944
Luca Spoldi <spoldi.luca06@outlook.it>
2025-04-18 10:04:33 +0200

    fixing small error

--- controllers/user.mjs
+++ controllers/user.mjs
@@ -88,6 +88,11 @@ export async function register(req, res) {
         return;
     }
 
+    // Remove '#' from the beginning of the colors if present
+    primary = primary.startsWith('#') ? primary.slice(1) : primary;
+    secondary = secondary.startsWith('#') ? secondary.slice(1) : secondary;
+    tertiary = tertiary.startsWith('#') ? tertiary.slice(1) : tertiary;
+
     // theme checks
     if (!isValidColor(primary) || !isValidColor(secondary) || !isValidColor(tertiary)) {
         sendError(res, "Theme colors are not valid");
